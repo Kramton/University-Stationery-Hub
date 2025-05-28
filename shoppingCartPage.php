568c682cace890e43a9412c91fe6341a38281824
TheEshoYousif <177274622+TheEshoYousif@users.noreply.github.com>
2025-05-28 18:59:53 +1200

    work in progress

--- shoppingCartPage.php
+++ shoppingCartPage.php
@@ -3,9 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homerchu</title>
+    <title>Shopping Cart</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    
+    <header class="cart-header">
+        <div class="logo">
+            <img src="" alt="Stationary hub Logo">
+        </div>
+        <h1 class="page-title">University Stationary Hub</h1>
+        <nav class="main-nav">
+            <div class="nav-links">
+                <a href="">Home</a>
+                <a href="">Products</a>
+                <a href="">Contact Us</a>
+                <a href="">Search</a>
+                <a href="">Log In</a>
+            </div>
+            <div class="cart">
+                <img src="" alt="shopping cart logo">
+            </div>
+            
+        </nav>
+    </header>
+    <main class="main">
+        <!-- <section class="orange-background">
+                <div class="orange-content">
+                    <h2>Welcome to University Stationary Hub</h2>
+                    <p>Find all your stationery needs in one place.</p>
+                <button>Shop Now</button>
+            </div>
+        </section> -->
+
+        <h2>Checkout</h2>
+    </main>
+    <footer></footer>
 </body>
 </html>
