3b1f5c4195df50874d7b06231a1d68294ac70946
vincentyu22 <132400993+vincentyu22@users.noreply.github.com>
2025-07-03 03:26:56 +1200

    59 - Single url

--- index.php
+++ index.php
@@ -160,7 +160,7 @@
 
           <h5 class="p-name"><?php echo $row ['product_name']; ?></h5>
           <h4 class="p-price"><?php echo $row ['product_price']; ?></h4>
-          <button class="buy-btn">Buy Now</button>
+          <a href="<?php echo "single_product.php?product_id=". $row['product_id'];?>"> <button class="buy-btn">Buy Now</button> </a>
         </div>
 
       
@@ -208,7 +208,7 @@
 
           <h5 class="p-name"><?php echo $row['product_name']; ?></h5>
           <h4 class="p-price"><?php echo $row['product_price']; ?></h4>
-          <button class="buy-btn">Buy Now</button>
+          <a href="<?php echo "single_product.php?product_id=". $row['product_id'];?>"> <button class="buy-btn">Buy Now</button> </a>
         </div>
 
         <?php } ?>
