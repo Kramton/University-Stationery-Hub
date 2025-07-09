799f0ac9ac9992ddaae5a26a910af35d65195aea
Kramton <markymoo719@gmail.com>
2025-07-09 21:03:51 +1200

    98 - testing orders

--- payment.php
+++ payment.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if(isset($_POST['order_pay_btn'])) {
+if (isset($_POST['order_pay_btn'])) {
   $order_status = $_POST['order_status'];
   $order_total_price = $_POST['order_total_price'];
 }
@@ -11,27 +11,27 @@ if(isset($_POST['order_pay_btn'])) {
 
 <?php include('layouts/header.php') ?>
 
-    <!-- Payment -->
-    <section class="my-5 py-5">
-      <div class="container text-center mt-3 pt-5">
-        <h2 class="form-weight-bold">Payment</h2>
-        <hr class="mx-auto" />
-      </div>
-      <div class="mx-auto container text-center">
-        
-        <p>Total payment: $<?php if(isset($_SESSION['total'])){ echo $_SESSION['total'];} ?></p>
-        <?php if(isset($_SESSION['total']) && $_SESSION['total'] != 0){ ?>
-        <!-- <input class="btn btn-primary" type="submit" value="Pay Now"> -->
-        <?php } else { ?>
-          <p>Your cart is empty!</p>
-        <?php } ?>
+<!-- Payment -->
+<section class="my-5 py-5">
+  <div class="container text-center mt-3 pt-5">
+    <h2 class="form-weight-bold">Payment</h2>
+    <hr class="mx-auto" />
+  </div>
+  <div class="mx-auto container text-center">
 
-        <p><?php if(isset($_POST['order_status'])) {echo $_POST['order_status'];} ?></p>
+    <?php if(isset($_SESSION['total']) && $_SESSION['total'] != 0) {?>
+      <p>Total payment: $<?php echo $_SESSION['total']; ?></p>
+      <input class="btn btn-primary" type="submit" value="Pay Now">
 
-        <?php if(isset($_POST['order_status']) && $_POST['order_status'] == "not paid") { ?>
-        <input class="btn btn-primary" type="submit" value="Pay Now">
-        <?php } ?>
-      </div>
-    </section>
+    <?php } else if(isset($_POST['order_status']) && $_POST['order_status'] == "not paid") {?>
+            <p>Total Payment: $<?php echo $_POST['order_total_price']; ?></p>
+            <input class="btn btn-primary" type="submit" value="Pay Now">
 
-  <?php include('layouts/footer.php') ?>
+    <?php } else { ?>
+        <p>Your cart is empty!</p>
+    <?php } ?>
+    
+  </div>
+</section>
+
+<?php include('layouts/footer.php') ?>
