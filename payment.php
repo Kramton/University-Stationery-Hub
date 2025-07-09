6ea73106fcb5420d4b0b3d09493578804cbd7d7c
Kramton <markymoo719@gmail.com>
2025-07-10 01:58:00 +1200

    107 + 108 - payment successful

--- payment.php
+++ payment.php
@@ -24,10 +24,10 @@ if (isset($_POST['order_pay_btn'])) {
 
     <?php if (isset($_POST['order_status']) && $_POST['order_status'] == "not paid") { ?>
       <?php $amount = strval($_POST['order_total_price']); ?>
-        <p>Total Payment: $<?php echo $_POST['order_total_price']; ?></p>
-        <!-- <input class="btn btn-primary" type="submit" value="Pay Now"> -->
-        <!-- Set up container element for the button -->
-        <div id="paypal-button-container"></div>
+      <p>Total Payment: $<?php echo $_POST['order_total_price']; ?></p>
+      <!-- <input class="btn btn-primary" type="submit" value="Pay Now"> -->
+      <!-- Set up container element for the button -->
+      <div id="paypal-button-container"></div>
 
 
 
@@ -36,10 +36,10 @@ if (isset($_POST['order_pay_btn'])) {
 
       <?php $amount = strval($_SESSION['total']); ?>
 
-      <p>Total payment: $<?php echo $_SESSION['total']; ?></p>
-      <!-- <input class="btn btn-primary" type="submit" value="Pay Now"> -->
-      <!-- Set up container element for the button -->
-      <div id="paypal-button-container"></div>
+        <p>Total payment: $<?php echo $_SESSION['total']; ?></p>
+        <!-- <input class="btn btn-primary" type="submit" value="Pay Now"> -->
+        <!-- Set up container element for the button -->
+        <div id="paypal-button-container"></div>
 
     <?php } else { ?>
         <p>Your cart is empty!</p>
@@ -52,37 +52,39 @@ if (isset($_POST['order_pay_btn'])) {
 </section>
 
 
-<script src="https://www.paypal.com/sdk/js?client-id=AYVYkWacTQ3tL6cxbCL_dadNr50VG2t0_Lhc3zKvlOA-h_tmHoAgQ7a7CdpkKcutRDig3PTuwYMrJP8c&currency=NZD"></script>
+<script
+  src="https://www.paypal.com/sdk/js?client-id=AYVYkWacTQ3tL6cxbCL_dadNr50VG2t0_Lhc3zKvlOA-h_tmHoAgQ7a7CdpkKcutRDig3PTuwYMrJP8c&currency=NZD"></script>
 
 <script>
-        paypal.Buttons({
-            createOrder: function(data, actions) {
-                return actions.order.create({
-                    purchase_units: [{
-                        amount: {
-                            value: '<?php echo $amount; ?>' // New Zealand Dollars
-                        }
-                    }]
-                });
-            },
-            onApprove: function(data, actions) {
-                return actions.order.capture().then(function(details) {
-                    alert('Transaction completed by ' + details.payer.name.given_name);
-                    
-                    // Optional: Send details to PHP server for verification or database logging
-                    fetch('payment_success.php', {
-                        method: 'post',
-                        headers: {
-                            'content-type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            orderID: data.orderID,
-                            payerID: data.payerID
-                        })
-                    });
-                });
-            }
-        }).render('#paypal-button-container');
-    </script>
+  paypal.Buttons({
+    createOrder: function (data, actions) {
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '<?php echo $amount; ?>' // New Zealand Dollars
+          }
+        }]
+      });
+    },
+    onApprove: function (data, actions) {
+      return actions.order.capture().then(function (details) {
+        
+        alert('Transaction completed by ' + details.payer.name.given_name);
+
+        // Optional: Send details to PHP server for verification or database logging
+        fetch('payment_success.php', {
+          method: 'post',
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            orderID: data.orderID,
+            payerID: data.payerID
+          })
+        });
+      });
+    }
+  }).render('#paypal-button-container');
+</script>
 
 <?php include('layouts/footer.php') ?>
