0a474511dc6ea1e0ea76f76649e8c2161105d8f5
qqmelonqq <105336178+qqmelonqq@users.noreply.github.com>
2025-07-22 13:46:14 +1200

    135-account

---
+++ admin/help.php
@@ -0,0 +1,32 @@
+ <?php include('sidemenu.php');?>
+
+
+
+
+
+     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+        <h1>Help</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+            <div class="btn-group mc-2">
+
+      </div>
+      
+    </div>
+</div>
+
+
+        <div class="container mt-3">
+            <p>Please contact admin@email.co,</p>
+            <p>Please cal 12345678</p>
+        </div>
+
+        </div>
+    </main>
+
+
+
+</div>
+
+
+<script src="https://rdn.jsdeiver.net.npm/bootstrap05.1.1/dist/js/bootstrap.bundle.min.js" intergrity="sha384
