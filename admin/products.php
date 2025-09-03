cc70616797171eec58ad60650c5428fa7389a4f7
Kramton <markymoo719@gmail.com>
2025-09-03 18:39:20 +1200

    added delete product confirmation

--- admin/products.php
+++ admin/products.php
@@ -12,8 +12,6 @@ if (!isset($_SESSION['admin_logged_in'])) {
 
 ?>
 
-
-
 <?php
 
 // get orders
@@ -62,61 +60,61 @@ $products = $stmt2->get_result();
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
         <h1>Dashboard</h1>
-        
+
       </div>
-      
+
     </main>
-    
+
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <hr class="mx-auto" />
       <h2>Products</h2>
 
       <!-- Edit product messages -->
-      <?php if(isset($_GET['edit_success_message'])) { ?>
+      <?php if (isset($_GET['edit_success_message'])) { ?>
         <p class="text-center" style="color: green;"><?php echo $_GET['edit_success_message']; ?></p>
       <?php } ?>
 
-      <?php if(isset($_GET['edit_failure_message'])) { ?>
+      <?php if (isset($_GET['edit_failure_message'])) { ?>
         <p class="text-center" style="color: red;"><?php echo $_GET['edit_failure_message']; ?></p>
       <?php } ?>
-      
-        <?php if(isset($_GET['stock_success_message'])) { ?>
-               <p class="text-center" style="color: green;"><?php echo htmlspecialchars($_GET['stock_success_message']); ?></p>
-             <?php } ?>
 
-       <?php if(isset($_GET['stock_failure_message'])) { ?>
-             <p class="text-center" style="color: red;"><?php echo htmlspecialchars($_GET['stock_failure_message']); ?></p>
-             <?php } ?>
+      <?php if (isset($_GET['stock_success_message'])) { ?>
+        <p class="text-center" style="color: green;"><?php echo htmlspecialchars($_GET['stock_success_message']); ?></p>
+      <?php } ?>
+
+      <?php if (isset($_GET['stock_failure_message'])) { ?>
+        <p class="text-center" style="color: red;"><?php echo htmlspecialchars($_GET['stock_failure_message']); ?></p>
+      <?php } ?>
 
 
 
 
       <!-- Delete product message -->
-      <?php if(isset($_GET['deleted_successfully'])) { ?>
+      <?php if (isset($_GET['deleted_successfully'])) { ?>
         <p class="text-center" style="color: green;"><?php echo $_GET['deleted_successfully']; ?></p>
       <?php } ?>
 
-      <?php if(isset($_GET['deleted_failure'])) { ?>
+      <?php if (isset($_GET['deleted_failure'])) { ?>
         <p class="text-center" style="color: red;"><?php echo $_GET['deleted_failure']; ?></p>
       <?php } ?>
 
       <!-- Create product -->
-      <?php if(isset($_GET['product_created'])) { ?>
+      <?php if (isset($_GET['product_created'])) { ?>
         <p class="text-center" style="color: green;"><?php echo $_GET['product_created']; ?></p>
       <?php } ?>
 
-      <?php if(isset($_GET['product_failed'])) { ?>
+      <?php if (isset($_GET['product_failed'])) { ?>
         <p class="text-center" style="color: red;"><?php echo $_GET['product_failed']; ?></p>
       <?php } ?>
 
       <!-- Update images -->
-      <?php if(isset($_GET['images_updated'])) { ?>
+      <?php if (isset($_GET['images_updated'])) { ?>
         <p class="text-center" style="color: green;"><?php echo $_GET['images_updated']; ?></p>
       <?php } ?>
 
-      <?php if(isset($_GET['images_failed'])) { ?>
+      <?php if (isset($_GET['images_failed'])) { ?>
         <p class="text-center" style="color: red;"><?php echo $_GET['images_failed']; ?></p>
-      <?php } ?>      
+      <?php } ?>
 
 
 
@@ -128,7 +126,7 @@ $products = $stmt2->get_result();
               <th scope="col">Product Image</th>
               <th scope="col">Product Name</th>
               <th scope="col">Product Price</th>
-               <th scope="col">Stock</th>
+              <th scope="col">Stock</th>
               <th scope="col">Product Offer</th>
               <th scope="col">Product Category</th>
               <th scope="col">Product Color</th>
@@ -141,46 +139,53 @@ $products = $stmt2->get_result();
           </thead>
           <tbody>
 
-            <?php foreach($products as $product) { ?>
-            <tr>
-              <td><?php echo $product['product_id']; ?></td>
-              <td><img src="<?php echo "../assets/imgs/". $product['product_image']; ?>" style="width: 70px; height: 70px;"></td>
-              <td><?php echo $product['product_name']; ?></td>
-              <td><?php echo "$" . $product['product_price']; ?></td>
-              
-            <td>
-            <?php $stock = isset($product['product_stock']) ? (int)$product['product_stock'] : 0;
-            // color code
-            if ($stock === 0) 
-            { $badge = 'bg-secondary';} 
-             elseif ($stock <= 5) 
-            { 
-             $badge = 'bg-danger';              // low
-           } 
-        elseif ($stock <= 20) 
-           {
-            $badge = 'bg-warning text-dark';   // medium
-           } 
-          else 
-            { $badge = 'bg-success';             // high
-              }   ?>
-              <span class="badge <?php echo $badge; ?>"><?php echo $stock; ?></span> </td>
+            <?php foreach ($products as $product) { ?>
+              <tr>
+                <td><?php echo $product['product_id']; ?></td>
+                <td><img src="<?php echo "../assets/imgs/" . $product['product_image']; ?>"
+                    style="width: 70px; height: 70px;"></td>
+                <td><?php echo $product['product_name']; ?></td>
+                <td><?php echo "$" . $product['product_price']; ?></td>
 
-              
-            
-              
-              <td><?php echo $product['product_special_offer'] . "%"; ?></td>
-              <td><?php echo $product['product_category']; ?></td>
-              <td><?php echo $product['product_color']; ?></td>
+                <td>
+                  <?php $stock = isset($product['product_stock']) ? (int) $product['product_stock'] : 0;
+                  // color code
+                  if ($stock === 0) {
+                    $badge = 'bg-secondary';
+                  } elseif ($stock <= 5) {
+                    $badge = 'bg-danger';              // low
+                  } elseif ($stock <= 20) {
+                    $badge = 'bg-warning text-dark';   // medium
+                  } else {
+                    $badge = 'bg-success';             // high
+                  } ?>
+                  <span class="badge <?php echo $badge; ?>"><?php echo $stock; ?></span>
+                </td>
 
-              <td><a class="btn btn-warning" href="<?php echo "edit_images.php?product_id=" . $product['product_id'] . "&product_name=" . $product['product_name']; ?>">Edit Images</a></td>
-              <td><a class="btn btn-primary" href="edit_product.php?product_id=<?php echo $product['product_id']; ?>">Edit</a></td>
-              <td><a class="btn btn-danger" href="delete_product.php?product_id=<?php echo $product['product_id'] ?>">Delete</a></td>
-           
-            </tr>
+
+
+
+                <td><?php echo $product['product_special_offer'] . "%"; ?></td>
+                <td><?php echo $product['product_category']; ?></td>
+                <td><?php echo $product['product_color']; ?></td>
+
+                <td><a class="btn btn-warning"
+                    href="<?php echo "edit_images.php?product_id=" . $product['product_id'] . "&product_name=" . $product['product_name']; ?>">Edit
+                    Images</a></td>
+                <td><a class="btn btn-primary"
+                    href="edit_product.php?product_id=<?php echo $product['product_id']; ?>">Edit</a></td>
+                <td>
+                  <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal"
+                    data-product-name="<?php echo htmlspecialchars($product['product_name']); ?>"
+                    data-delete-url="delete_product.php?product_id=<?php echo $product['product_id']; ?>">
+                    Delete
+                  </button>
+                </td>
+
+              </tr>
 
             <?php } ?>
-            
+
           </tbody>
         </table>
 
@@ -223,6 +228,45 @@ $products = $stmt2->get_result();
 
 
       </div>
+
+      <!-- Center modal-header -->
+      <style>
+        .modal-header {
+          justify-content: center;
+        }
+
+        .modal-header .btn-close {
+          position: absolute;
+          right: 1rem;
+          top: 1rem;
+        }
+
+        .modal-footer #cancelBtn, .modal-footer #confirmDeleteBtn {
+          padding-left: 50px;
+          padding-right: 50px;
+          
+        }
+      </style>
+
+      <!-- Delete Confirmation Modal -->
+      <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h2 class="modal-title" id="deleteModalLabel"><b>Delete Product</b></h2>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+              Are you sure you want to delete "<span id="modalProductName"></span>"?
+            </div>
+            <div class="modal-footer justify-content-center">
+              <button id="cancelBtn" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+              <a href="#" id="confirmDeleteBtn" class="btn btn-danger ms-2">Delete</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </main>
 
 
@@ -233,9 +277,22 @@ $products = $stmt2->get_result();
 
 
 <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
   integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"
   integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
 <script src="dashboard.js"></script>
+
+<script>
+  // Handle modal data population
+  var deleteModal = document.getElementById('deleteModal');
+  deleteModal.addEventListener('show.bs.modal', function (event) {
+    var button = event.relatedTarget;
+    var productName = button.getAttribute('data-product-name');
+    var deleteUrl = button.getAttribute('data-delete-url');
+    document.getElementById('modalProductName').textContent = productName;
+    document.getElementById('confirmDeleteBtn').setAttribute('href', deleteUrl);
+  });
+</script>
