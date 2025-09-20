179a4803f13040984a973448aed95df2c12957ff
TheEshoYousif <177274622+TheEshoYousif@users.noreply.github.com>
2025-09-20 18:42:38 +1200

    Merge branch 'upskill' of https://github.com/Kramton/University-Stationary-Hub into upskill

--- forgot_password.php
+++ forgot_password.php
@@ -1,38 +1,40 @@
-
 <?php include('layouts/header.php') ?>
+
+<style>
+  .reset-btn {
+    background-color: coral;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    border-radius: 5px;
+    margin-top: 10px;
+  }
+</style>
 
 <!-- Reset Password -->
 <section id="contact" class="container my-5 py-5">
-    <div class="container text-center mt-5">
-        <h3>Reset Password</h3>
-        <hr class="mx-auto" />
+  <div class="container text-center mt-5">
+    <h2 class="form-weight-bold">Forgot Password</h2>
+    <hr class="mx-auto" />
 
-        <form method="POST" action="send_password_reset.php">
+  </div>
 
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email">
+  <form class="container text-center" method="POST" action="send_password_reset.php">
+    <div>
+      <p>Enter your email and we'll send a link to reset your password.</p>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-4">
+        <div class="d-grid gap-2">
+          <input required type="email" name="email" id="email" placeholder="Enter your email" class="form-control">
+          <button class="reset-btn">Reset Password</button>
+        </div>
+      </div>
+    </div>
+  </form>
 
-            <button>Send</button>
-
-        </form>
-
-        <!-- <p class="w-50 mx-auto">Phone number: <span>123 456 7890</span></p>
-        <p class="w-50 mx-auto">
-          Email Address: <span>universitystationaryhub@mail.com</span>
-        </p>
-        <p class="w-50 mx-auto">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-          omnis aperiam magni facere cupiditate laudantium, laboriosam, cum,
-          voluptates vero recusandae dolores repellat doloribus porro? Provident
-          consequuntur quisquam sapiente adipisci error!
-        </p>
-      </div> -->
 </section>
 
 <!-- Footer -->
 <?php include('layouts/footer.php') ?>
-
-
-<?php
-
-?>
--- reset_password.php
+++ reset_password.php
@@ -25,28 +25,41 @@ if(strtotime($user["reset_token_expires_at"]) <= time()) {
 
 ?>
 
+<style>
+  .confirm-btn {
+    background-color: coral;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+    border-radius: 5px;
+    margin-top: 10px;
+  }
+</style>
 
 <?php include('layouts/header.php') ?>
 
 <!-- Reset Password -->
 <section id="contact" class="container my-5 py-5">
     <div class="container text-center mt-5">
-        <h3>Reset Password</h3>
+        <h2>Reset Password</h2>
         <hr class="mx-auto" />
+        <p>Please enter a new password.</p>
 
         <form method="POST" action="process_reset_password.php">
             <input type="hidden" name="token" value="<?= htmlspecialchars($token) ?>">
-            
-            <p>Password</p>
-            <label for="password"></label>
-            <input type="text" id="password" name="password">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-4">
+                    <div class="d-grid gap">
+                        Password<input required type="password" name="password" id="password" placeholder="Enter new password" class="form-control mb-3">
+                        Confirm Password<input required type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirm new password" class="form-control">
+                        <button class="confirm-btn">Confirm</button>
+                    </div>
+                </div>
 
-            <p>Confirm Password</p>
-            <label for="password_confirmation"></label>
-            <input type="text" id="password_confirmation" name="password_confirmation">
-
-            <button>Send</button>
+            </div>
         </form>
+    </div>
 
 </section>
 
