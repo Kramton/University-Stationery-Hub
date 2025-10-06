2ad7743b8188dcb303004496865b82fa9a958cd8
Kramton <markymoo719@gmail.com>
2025-10-06 17:47:52 +1300

    fix side menu icons not displaying when adding new promo code

--- admin/add_promo_code.php
+++ admin/add_promo_code.php
@@ -92,5 +92,9 @@ if (isset($_POST['add_promo_code_btn'])) {
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://unpkg.com/feather-icons"></script>
+<script>
+  feather.replace();
+</script>
 </body>
 </html>
